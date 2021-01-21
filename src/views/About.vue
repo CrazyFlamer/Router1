3bde4094cad0deaad0554594cc26c9746dbd1e7a
CrazyFlamer <CrazyFlamer@github.com>
2021-01-21 19:50:59 +0300

    The end

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Здесь информация о проекте</h1>
   </div>
 </template>
---
+++ src/views/Inform.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="inform">
+    <h1>Это контакты о нас</h1>
+  </div>
+</template>
